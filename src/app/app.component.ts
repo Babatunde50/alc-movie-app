d9e4c3ef8fb3cc682f50c02dac412ad13fe46d60
Babatunde50 <babatundeololade50@gmail.com>
2019-10-15 05:54:50 +0100

    added the header component and setup angular http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-app';
+
+  constructor(private http: HttpClient) {
+    // Send an http request
+    this.http.get('https://www.omdbapi.com/?s=man&apikey=7770e21c')
+      .subscribe(responseData => {
+        console.log(responseData);
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AllComponent } from './all/all.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AllComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
